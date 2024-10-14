67a1d1c0984181e7bc51698512dcdacf086e97cb
Caio Ricciuti <caio.ricciuti@outlook.com>
2024-10-14 22:37:56 +0200

    fix path into ui components docker-false no-release

--- docs/components/ui/carousel.tsx
+++ docs/components/ui/carousel.tsx
@@ -7,7 +7,7 @@ import useEmblaCarousel, {
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "./button"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
