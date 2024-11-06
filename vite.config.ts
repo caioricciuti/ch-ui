55ba824876e0569403e19efdc4fed3502b9b5ee6
Stan <Stanpol@users.noreply.github.com>
2024-11-06 21:58:34 +0400

    add inline files config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: '/ui/',
   resolve: {
     alias: {
